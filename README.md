71eb9628a1e093f72d347b3167dadb1878596598
starchie-d <dmitry.ianshin@gmail.com>
2021-02-10 14:24:07 +0300

    Init project. Closes #1.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # beer-microservice
+
 Spring Boot training project
---
+++ src/test/java/dev/yanshin/beermicroservice/BeerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yanshin.beermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
